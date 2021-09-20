a9b37f6aa8906edaf9e04b17d185c26dd895e5a9
rhobbikhaidir <rhobbie09@gmail.com>
2021-09-20 14:44:37 +0700

    finish react-max-section-9

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -23,7 +23,7 @@ const AddUser = (props) => {
     if (+enteredUserAge < 1) {
       setError({
         title: "Invalid age",
-        message: "Please enter a valid age (> 0).",
+        message: "Please enter a valid age (> 0)",
       });
       return;
     }
